c5bf73a809d6b61a7363c742958d43c5337afa0a
Saipriyakcybage <153483603+Saipriyakcybage@users.noreply.github.com>
2026-04-22 17:46:40 +0530

    Add files via upload

---
+++ AdminOrdersController.cs
@@ -0,0 +1,89 @@
+﻿using Library.ModelDtos;
+using Library.Models;
+using Library.Services;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace Library.Controllers
+{
+    [Authorize(Roles = "admin")]
+    [Route("/AdminOrders/{action=Index}/{id?}")]
+
+    public class AdminBorrowedBooksController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly Microsoft.AspNetCore.Identity.UserManager<ApplicationUser> _userManager;
+        private readonly int _pageSize = 5;
+
+        public AdminBorrowedBooksController(Microsoft.AspNetCore.Identity.UserManager<ApplicationUser> userManager, ApplicationDbContext context)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        public IActionResult Index(int pageIndex = 1)
+        {
+            IQueryable<Borrow> query = _context.Borrows.Include(b => b.User)
+                                                       .Include(b => b.Product)
+                                                       .OrderByDescending(b => b.borrowedDate);
+
+            if (pageIndex <= 0)
+            {
+                pageIndex = 1;
+            }
+
+            decimal count = query.Count();
+            int totalPages = (int)Math.Ceiling(count / _pageSize);
+
+            query = query.Skip((pageIndex - 1) * _pageSize).Take(_pageSize);
+            var borrowedBooks = query.ToList();
+
+            ViewBag.BorrowedBooks = borrowedBooks;
+            ViewBag.PageIndex = pageIndex;
+            ViewBag.TotalPages = totalPages;
+
+            return View();
+        }
+
+        public IActionResult Details(int id)
+        {
+            var borrow = _context.Borrows.Include(b => b.User)
+                                         .Include(b => b.Product)
+                                         .FirstOrDefault(b => b.Id == id);
+
+            if (borrow == null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return View(borrow);
+        }
+
+        public IActionResult Notification()
+        {
+            var notification = _context.Notifications.ToList();
+            return View(notification);
+        }
+        [HttpPost]
+        public IActionResult AcceptRequest(int Id)
+        {
+           
+
+            var notification = _context.Notifications.FirstOrDefault(b => b.Id == Id)!;
+            var borrow = _context.Borrows.FirstOrDefault(b => b.Id == notification.BorrowId)!;
+            borrow.returnedDate = notification.RequestDate;
+            if (borrow.dateTobereturn < notification.RequestDate)
+            {
+                borrow.fine += (DateTime.Now - borrow.dateTobereturn).Days;
+            }
+            _context.Update(borrow);
+            _context.Remove(notification);
+            _context.SaveChanges();
+            return RedirectToAction("Notification");
+        }
+    }
+}
---
+++ ReturnBookController.cs
@@ -0,0 +1,87 @@
+﻿using Library.ModelDtos;
+using Library.Services;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Library.Models
+{
+    [Authorize(Roles ="admin")]
+    public class ReturnBookController : Controller
+    {
+        private readonly Microsoft.AspNetCore.Identity.UserManager<ApplicationUser> _userManager;
+        private readonly ApplicationDbContext _context;
+
+        public ReturnBookController(Microsoft.AspNetCore.Identity.UserManager<ApplicationUser> userManager, ApplicationDbContext context)
+        {
+            _userManager = userManager;
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var returnedBooks = _context.ReturnBooks
+                .Include(r => r.Product)
+                .Include(r => r.User)
+                .ToList();
+
+            return View(returnedBooks);
+        }
+
+        public IActionResult AddReturnItem()
+        {
+            return View(new ReturnBookDto());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddReturnItem(ReturnBookDto book)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByNameAsync(book.Username);
+                if (user == null)
+                {
+                    ModelState.AddModelError("", $"User with username '{book.Username}' not found.");
+                    return View(book);
+                }
+
+                var updateborrow = _context.Borrows.FirstOrDefault(b => b.UserId == user.Id && b.ProductId == book.ProductId && b.returnedDate==null);
+                if (updateborrow == null)
+                {
+                    ModelState.AddModelError("", $"User '{book.Username}' has not borrowed the book with Product ID '{book.ProductId}', or it has already been returned.");
+                    return View(book);
+                }
+                if (updateborrow != null)
+                {
+                    updateborrow.returnedDate = DateTime.Now;
+
+                    if (updateborrow.dateTobereturn < DateTime.Now)
+                    {
+                        updateborrow.fine += (DateTime.Now - updateborrow.dateTobereturn).Days;
+                    }
+
+                    _context.Update(updateborrow);
+
+                    var returnEntry = new ReturnBook
+                    {
+                        Userid = user.Id,
+                        ProductId = book.ProductId,
+                        ReturnedDate = DateTime.Now
+                    };
+
+                    _context.ReturnBooks.Add(returnEntry);
+                    _context.SaveChanges();
+                }
+
+                return RedirectToAction("Index");
+            }
+
+            return View(book);
+        }
+        
+
+
+
+    }
+}
