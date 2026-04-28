12fa2eb9bb9530827aefcf7dcaff9f03670940be
Saipriyakcybage <153483603+Saipriyakcybage@users.noreply.github.com>
2026-04-28 11:45:35 +0530

    Add files via upload

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Library.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Library.Services
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions options): base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<AddToCart> AddToCarts { get; set; }
+        public DbSet<Borrow> Borrows { get; set; }
+        public DbSet<ReturnBook> ReturnBooks {  get; set; }
+
+        public DbSet<Notification> Notifications { get; set; }
+    }
+}
